dfd14b7f155378d9e3d3067e5a6c6961a5bb34e0
Personal-Vijayji1137 <personal.vijayji1137@gmail.com>
2024-11-03 15:10:19 +0530

    Lit

--- src/app/server/uploadvideos.js
+++ src/app/server/uploadvideos.js
@@ -23,19 +23,20 @@ export default async function UploadVideosToS3(video_url, movie, format, id) {
         const postData = {
             video_url: video_url,
             upload_url: url,
-        };
-        await fetch('https://iplustsolution-uploadmovie.hf.space/upload-movies', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(postData),
-        });
-        await Root_Movies_DB(
-            `INSERT INTO movie_links (movie_id, url, quality) VALUES (?, ?, ?)`,
-            [id, fileName, format]
-        );
-        return { fileName }
+        }
+        return postData
+        // await fetch('https://iplustsolution-uploadmovie.hf.space/upload-movies', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify(postData),
+        // });
+        // await Root_Movies_DB(
+        //     `INSERT INTO movie_links (movie_id, url, quality) VALUES (?, ?, ?)`,
+        //     [id, fileName, format]
+        // );
+        // return { fileName }
     } catch (err) {
         return { type: 'error', err: err };
     }
